475e2397cb6d250fa9b35f0abcaa7941d7ddb3ec
74956 <111351531+74956@users.noreply.github.com>
2023-02-02 15:00:41 +0100

    Create generateAllureReport.groovy

---
+++ vars/generateAllureReport.groovy
@@ -0,0 +1,9 @@
+def generateAllure() {
+    allure([
+            includeProperties: true,
+            jdk              : '',
+            properties       : [],
+            reportBuildPolicy: 'ALWAYS',
+            results          : [[path: 'target/allure-results']]
+    ])
+}
