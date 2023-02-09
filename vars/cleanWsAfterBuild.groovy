d2fae8e466b85f8a3e56a430a8d0361fad6fa244
74956 <111351531+74956@users.noreply.github.com>
2023-02-09 09:15:14 +0100

    Delete cleanWsAfterBuild.groovy

--- vars/cleanWsAfterBuild.groovy
+++
@@ -1,6 +0,0 @@
-cleanWs(cleanWhenNotBuilt: false,
-       deleteDirs: true,
-       disableDeferredWipeout: true,
-       notFailBuild: true,
-       patterns: [[pattern: '.gitignore', type: 'INCLUDE'],
-                 [pattern: '.propsfile', type: 'EXCLUDE']])
